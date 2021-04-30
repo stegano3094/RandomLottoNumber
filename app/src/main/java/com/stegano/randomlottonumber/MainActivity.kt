a04793eee3d3be835fd2fa5dbb88af81bac27e61
stegano <steganowork@naver.com>
2021-04-30 21:16:46 +0900

    'commit2'

--- app/src/main/java/com/stegano/randomlottonumber/MainActivity.kt
+++ app/src/main/java/com/stegano/randomlottonumber/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.stegano.randomlottonumber
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<CardView>(R.id.randomCard).setOnClickListener {
+            val intent = Intent(this@MainActivity, ResultActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<CardView>(R.id.constellationCard).setOnClickListener {
+            val intent = Intent(this@MainActivity, ConstellationActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<CardView>(R.id.nameCard).setOnClickListener {
+            val intent = Intent(this@MainActivity, NameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
